3e73054e314721a6868a6ac8b6e9fe01d3eb32eb
Castrew <141501794+Castrew@users.noreply.github.com>
2024-09-26 17:10:12 +0300

    Google password popup requirements tricked
    - These changes were done because Google chrome is checking the hardness of passwords that are typed and there was a popup that was problem for the tests. With this harder password the popup is gone and the tests are working fine.

--- test-cafe/signInTest.js
+++ test-cafe/signInTest.js
@@ -3,8 +3,8 @@ import { Selector } from "testcafe";
 fixture`register`;
 
 test.page`http://localhost:3000`("test1", async (t) => {
-  await t.typeText(Selector("#sign-in-username-input"), "bbbbb");
-  await t.typeText(Selector("#sign-in-password-input"), "aaaaaaaa");
+  await t.typeText(Selector("#sign-in-username-input"), "sexfactor");
+  await t.typeText(Selector("#sign-in-password-input"), "mzm9tpj6ECK_avp*txg");
   await t.click(Selector("#submit-sign-in-button")).wait(5000);
   console.log("im in ");
 });
--- test-cafe/signUpTest.js
+++ test-cafe/signUpTest.js
@@ -4,9 +4,12 @@ fixture`sign-up`;
 
 test.page`http://localhost:3000`("test1", async (t) => {
   await t.click(Selector("#sign-up-page-button"));
-  await t.typeText(Selector("#sign-up-username-input"), "bbbbb");
-  await t.typeText(Selector("#sign-up-password-input"), "aaaaaaaa");
-  await t.typeText(Selector("#sign-up-confirmPassword-input"), "aaaaaaaa");
+  await t.typeText(Selector("#sign-up-username-input"), "sexfactor");
+  await t.typeText(Selector("#sign-up-password-input"), "mzm9tpj6ECK_avp*txg");
+  await t.typeText(
+    Selector("#sign-up-confirmPassword-input"),
+    "mzm9tpj6ECK_avp*txg"
+  );
   await t.click(Selector("#submit-sign-up-button")).wait(5000);
   console.log("im in ");
 });
